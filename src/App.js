fe20886bd19f8701454e5469b29db8b709e24b92
selma <selmakurtovic@live.com>
2023-09-10 18:36:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Person from './components/Person'
+import Filter from './components/Filter'
+import PersonsForm from './components/PersonsForm'
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
-function App() {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [newFilter, setNewFilter] =useState('')
+  const [filteredPersons, setNewFilteredPersons]=useState(persons)
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const addPerson = (event) => {
+    console.log(newName)
+    event.preventDefault();
+    const foundPerson=persons.find(person=> JSON.stringify(person.name)===JSON.stringify(newName) );
+    if(foundPerson)
+     window.alert(`${newName} is already added to phonebook`);
+    else{
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+    setPersons(persons.concat(personObject))
+    setNewFilteredPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleFilterChange =(event) => {
+    const newFilterValue = event.target.value;
+    setNewFilter(newFilterValue);
+    let newArray=persons.filter(person=> person.name.toUpperCase().startsWith(newFilterValue.toUpperCase()))
+    setNewFilteredPersons(newArray);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}></Filter>
+      <h2>Add a new</h2>
+      <PersonsForm  handleNameChange={handleNameChange} newName={newName} addPerson={addPerson} newNumber={newNumber} handleNumberChange={handleNumberChange} ></PersonsForm>
+      <h2>Numbers</h2>
+      <ul>
+      {filteredPersons.map(person => 
+          <Person key={person.name} person={person}/>
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Filter.js
@@ -0,0 +1,7 @@
+const Filter = ({newFilter, handleFilterChange}) =>{
+return(<div>
+filter shown with
+<input value={newFilter} onChange={handleFilterChange}/>
+</div>);
+}
+export default Filter;
